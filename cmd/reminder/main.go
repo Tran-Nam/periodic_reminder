1000cad43f0c5a8b28a1c34f1d04e2fc359204b0
phuongnam <31546358+Tran-Nam@users.noreply.github.com>
2026-01-10 07:58:04 +0000

    use env from secret

--- cmd/reminder/main.go
+++ cmd/reminder/main.go
@@ -4,12 +4,9 @@ import (
 	"log"
 
 	"github.com/Tran-Nam/periodic_remind/pkg/telegram"
-	"github.com/Tran-Nam/periodic_remind/pkg/common"
 )
 
 func main() {
-	// load .env (optional) and send a test message
-	_ = common.LoadDotEnv(".env")
 	if err := telegram.SendMessageFromEnv("Good afternoon!"); err != nil {
 		log.Fatal(err)
 	}
